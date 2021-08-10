f74ab98dfd4cdac92027b10ecf707c54049b8f81
n00nietzsche <n00nietzsche@gmail.com>
2021-08-10 22:55:53 +0900

    스프링 MVC 2편 - 타임리프 기본 기능 #5 기본 객체들

--- src/main/java/mvc2/thymeleaf/controller/BasicController.java
+++ src/main/java/mvc2/thymeleaf/controller/BasicController.java
@@ -1,9 +1,17 @@
 package mvc2.thymeleaf.controller;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 // thymeleaf가 package 이름인거 까먹었었음..
 // `@SpringBootApplication` 애노테이션과 같은 레벨에서 만들어줘야
@@ -22,6 +30,52 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "/basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> users = new ArrayList<>();
+        users.add(userA);
+        users.add(userB);
+
+        Map<String, User> userMap = new HashMap<>();
+        userMap.put("userA", userA);
+        userMap.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", users);
+        model.addAttribute("userMap", userMap);
+
+        return "/basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        // `session`은 일반적으로 유저가 웹브라우저를 종료하기 전까지 유지되는 데이터이다.
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    // 테스트용 스프링 빈을 만드는 것
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
 
