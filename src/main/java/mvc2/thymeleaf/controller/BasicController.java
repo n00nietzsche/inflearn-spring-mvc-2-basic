64b10d86ec8412ab7a2f6775b9904792a97b38db
n00nietzsche <n00nietzsche@gmail.com>
2021-08-11 19:35:18 +0900

    스프링 MVC 2편 - 타임리프 기본 기능 #6 유틸리티 객체와 날짜

--- src/main/java/mvc2/thymeleaf/controller/BasicController.java
+++ src/main/java/mvc2/thymeleaf/controller/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -59,7 +60,14 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
     // 테스트용 스프링 빈을 만드는 것
+    // 빈이란 건 결국 하나의 객체를 그냥 싱글톤 레지스트리에 올리는 것
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
